6e8742959726a64b348ea534147963e04513b234
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-06-29 12:16:11 -0400

    Avances 29062021

--- js/script.js
+++ js/script.js
@@ -1,32 +1,98 @@
+// Declación de variables
+let nombre;
+let saludar;
+let error0 = 'Por favor ingresa un nombre de usuario.'
+let error1 = 'Por favor ingresa una respuesta valida.';
+let error2 = 'Por favor ingresa un número del 1 al 10.';
+let error3 = 'Por favor ingresa un correo valido.'
+let exito1 = 'Gracias por tu tiempo. Esta información será de gran utilidad para mejorar tu experiencia.';
+let exito2 = 'Gracias tu correo fue enviado con éxito. Hasta pronto!'
+let indicacion1 = ' Bienvenido(a) a Daely, una aplicación para aprender ingles de forma fácil y rápida.';
+let indicacion2 = 'Antes de comenzar, por favor tomate un minuto para responder a las siguientes preguntas. Esto mejorará tu experiencia de usuario.';
+
+let pregunta1;
+let pregunta2;
+let pregunta3;
+let correo;
+
+
 // Inicio
-// Entrada
-let nombre = prompt('Ingresa tu nombre')
+// Inicialización de variable nombre.
+nombre = prompt('Por favor ingresa tu nombre o un nombre de usuario.');
 
-// Proceso
-let saludar = 'Hola ' + nombre;
-let indicacion01 = saludar + '.' + ' Bienvenido(a) a Daely, una aplicación para aprender ingles de forma fácil y rápida.';
-let indicacion02 = 'Por favor, responde a las siguientes preguntas para evaluar tu nivel inicial. No hagas trampa es muy importante que respondas con sinceridad.';
-let nivelActual = 'Haz contestado a 3 de 3. Bien Hecho. Los siguientes modulos estarán disponibles proximamente.';
-let despedida = 'Gracias por tu tiempo.';
+// Validación de nombre.
+if (nombre === '') {
+  alert(error0);
 
-// Salida
-alert(indicacion01);
-alert(indicacion02);
-let pregunta01 = Number(prompt('1- Del 1 al 10 ¿Cuál es tu nivel de conocimiento del idioma Ingles?'));
-let pregunta02 = prompt('2- ¿En qué aspectos del idioma Ingles te gustaría mejorar?');
-let pregunta03 = prompt('3- ¿Cuál es tu objetivo con respecto al idioma Ingles?');
-alert(nivelActual);
-let correo = prompt('Si quieres que te hagamos llegar las novedades a tu correo. Puedes escribirlo en la casilla a continuación:');
-alert(despedida)
+} else {
+  // Inicialización de variable saludar.
+  saludar = 'Hola ' + nombre + '.';
 
+  // Saludar y dar indicación inicial. 
+  alert(saludar + indicacion1);
+  
+  // Indicación 2
+  alert(indicacion2);
+  
+  // Pregunta 1.
+  // Inicialización de variable pregunta1.
+  pregunta1 = Number(prompt('1- Del 1 al 10 ¿Cuál es tu nivel de conocimiento del idioma Ingles?'));
+  
+  // Validación de pregunta 1.
+  if(pregunta1 === ''){
+    alert(error2);
 
+  } else if(isNaN(pregunta1)) {
+    alert(error2);
+
+  } else if(pregunta1 < 1 || pregunta1 > 10) {
+    alert(error2);
+
+  } else {
+    // Pregunta 2.
+    // Inicialización de variable pregunta2.
+    pregunta2 = prompt('2- ¿En qué aspectos del idioma Ingles te gustaría mejorar?');
+    
+    // Validación de pregunta 2.
+    if (pregunta2 === '') {
+      alert(error1);
+    
+    } else {
+      // Pregunta 3.
+      // Inicialización de variable pregunta 3.
+      pregunta3 = prompt('3- ¿Cuál es tu objetivo con respecto al idioma Ingles?');
+      
+      // Validación de pregunta 3.
+      if (pregunta3 === '') {
+        alert(error1);
+      
+      } else {
+        alert(exito1);
+
+        // Petición de correo.
+        // Inicialización de variable correo.
+        correo = prompt('Esta aplicación aún esta en desarrollo, pero si quieres que te hagamos llegar las novedades a tu correo. Puedes escribirlo en la casilla a continuación:');
+        
+        // Validación de correo.
+        if (correo === '') {
+          alert(error3);
+        
+        } else {
+          alert(exito2);
+        };
+      };
+    };   
+  };
+}
+  
 // Comprobación de datos
-console.log(nombre);
-console.log(pregunta01);
-console.log(pregunta02);
-console.log(pregunta03);
-console.log(correo);
+console.log('Nombre: ' + nombre);
+console.log('Correo: ' + correo);
+console.log('Respuesta1: ' + pregunta1);
+console.log('Respuesta2: ' + pregunta2);
+console.log('Respuesta3: ' + pregunta3);
 
 
 // Fin
+
 
